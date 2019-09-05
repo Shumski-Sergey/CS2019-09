30499b6e63c3539a1c2f472c3f55f7570e55ee74
Yury6720 <kuzminskij5683415@gmail.com>
2019-09-05 21:20:14 +0300

    Lab1 2019-09-05 add TaskA1 add TaskA2 add TaskA3

---
+++ src/ykuzminskij/Lab1/TaskA3.java
@@ -0,0 +1,10 @@
+package ykuzminskij.Lab1;
+
+public class TaskA3 {
+    public static void main (String [] args) {
+        int i=4;
+        int j=3;
+        int k=i*i+j*j;
+        System.out.println (j +"*" + j + "+" + i + "*" + i +  "=" + k);
+    }
+}
