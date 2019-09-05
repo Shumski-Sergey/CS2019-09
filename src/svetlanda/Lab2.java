17540e637ef367bda04358c0dd6340da5f937ac6
svetlanda <Shumeiko01@mail.ru>
2019-09-05 19:20:09 +0300

    lesson 1 и 2

--- src/svetlanda/Lab2.java
+++ src/svetlanda/Lab2.java
@@ -3,10 +3,10 @@ package svetlanda;
 public class Lab2 {
 
     public static void main(String[] args) {
-        int i =3;
-        int j=4;
-        int k =i*j;
-        System.out.println(i+"*"+j+"="+k);
+        int i =3*3;
+        int j=4*4;
+        int k =i+j;
+        System.out.println(i+"+"+j+"="+k);
 
     }
 
