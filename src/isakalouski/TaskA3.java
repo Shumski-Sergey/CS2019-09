2745062ae0f99ba929118487f0c1cebc5c613adc
ilya-sakalouski <ilya6697089@mail.ru>
2019-09-05 21:31:02 +0300

    add lab1, lab2, lab3

---
+++ src/isakalouski/TaskA3.java
@@ -0,0 +1,10 @@
+package isakalouski;
+
+public class TaskA3 {
+    public static void  main (String[] args) {
+        int i = 4;
+        int j = 3;
+        int k = 25;
+        System.out.println(j + "*" + j + "+" + i + "*" + i + "=" + k );
+    }
+}
---
+++ src/isakalouski/main.java
@@ -0,0 +1,10 @@
+package isakalouski;
+
+public class main {
+    public static void main(String[] args) { System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+    }
+}
