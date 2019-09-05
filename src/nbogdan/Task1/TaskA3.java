18c44244d7cbe3a9ce162d2b8fbf50b8416112b9
PizzzaDog <7883494@gmail.com>
2019-09-05 20:59:48 +0300

    add

--- src/nbogdan/Task1/TaskA3.java
+++ src/nbogdan/Task1/TaskA3.java
@@ -3,9 +3,6 @@ package nbogdan.Task1;
 public class TaskA3 {
 
     public static void main(String[] args) {
-        int i = 4;
-        int j = 3;
-        int k = j*j+i*i;
-        System.out.println(j + "*" + j + "+" + i + "*" + i + "=" + k);
+        System.out.println("Hi!");
     }
 }
