72424073d8751abd155ff4466b27fb6a2e8542bc
sshumsky <shumski.s.a@gmail.com>
2019-09-05 21:32:50 +0300

    Filed class commented

--- src/aflyer/FirstLesson/TaskB1.java
+++ src/aflyer/FirstLesson/TaskB1.java
@@ -2,9 +2,9 @@ package aflyer.FirstLesson;
 
 public class TaskB1 {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        sc.newInt(int a);
-        System.out.println("Квадрат числа" + a + "составляет" + a*a)
+//        Scanner sc=new Scanner(System.in);
+//        sc.newInt(int a);
+//        System.out.println("Квадрат числа" + a + "составляет" + a*a)
 
     }
 }
