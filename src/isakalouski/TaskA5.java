56a8c258e0db96af3c4e55271815a392608f6e0f
ilya-sakalouski <ilya6697089@mail.ru>
2019-09-06 18:43:06 +0300

    Задания А и В

---
+++ src/isakalouski/TaskA5.java
@@ -0,0 +1,12 @@
+package isakalouski;
+
+public class TaskA5 {
+    private static int a = 1;
+    private static int b = 3;
+    private static int c = 9;
+    private static int d = 27;
+    public static void main(String[] args) {
+        int result = d + b - a - c;
+        System.out.println(result);
+    }
+}
---
+++ src/isakalouski/TaskA6.java
@@ -0,0 +1,7 @@
+package isakalouski;
+
+public class TaskA6 {
+    public static void main(String[] args) {
+        System.out.println("\"С Новым Годом\"");
+    }
+}
