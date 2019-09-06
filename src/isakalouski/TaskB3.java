d117c9f16864854eac2d4a53fad9e58f70cfe079
ilya-sakalouski <ilya6697089@mail.ru>
2019-09-06 19:13:08 +0300

    Задания А и В

--- src/isakalouski/TaskB3.java
+++ src/isakalouski/TaskB3.java
@@ -1,6 +1,6 @@
 package isakalouski;
 
-public class TaskA6 {
+public class TaskB3 {
     public static void main(String[] args) {
         String a = "\"С";
         System.out.println(a + " " + "Новым Годом\"");
