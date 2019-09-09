c610974b75b78ce1b74dd1896ea1469262458d8d
Flyer-jr <Flyer-jr@yandex.com>
2019-09-10 01:56:38 +0300

    added new tasks

---
+++ src/aflyer/SecondLesson/TaskA2.java
@@ -0,0 +1,12 @@
+package aflyer.SecondLesson;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        String a = "Хронику лет кладет протектор";
+        String b = "Кинопленкой на асфальт";
+        String c = "Компаса нет - случайный вектор";
+        String d = "Рвет горизонталь...";
+        System.out.println("Мое любимое стихотворение: \n" + a + "\n"+ b + "\n" +c + "\n"+ d);
+
+         }
+}
