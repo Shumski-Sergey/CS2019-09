9bff2c0f755b1ca91815cfe6eb8471f9d87c4be0
Yury6720 <kuzminskij5683415@gmail.com>
2019-09-10 02:12:01 +0300

    Program for counting quadratic equation

--- src/ykuzminskij/Lab2/TaskB2.java
+++ src/ykuzminskij/Lab2/TaskB2.java
@@ -11,29 +11,26 @@ public class TaskB2 {
         int b = sc.nextInt();
         System.out.println("Введите третье число");
         int c = sc.nextInt();
-        double res = dis(a, b, c);
-        double a1 = a;
-        double b1 = b;
-        if (res > 0) {
-            double x1 = (-b1 + Math.sqrt(res)) / (2 * a1);
-            double x2 = (-b1 - Math.sqrt(res)) / (2 * a1);
+        if (dis (a, b, c) > 0) {
+            double x1 = (-b + Math.sqrt(dis (a, b, c))) / (2 * a);
+            double x2 = (-b - Math.sqrt(dis (a, b, c))) / (2 * a);
             System.out.println("Первый корень " + x1);
             System.out.println("\n Второй корень " + x2);
         }
         else {
-              if (res==0){
-                  double x = (-b1 + Math.sqrt(res)) / (2 * a1);
+              if (dis (a, b, c)==0){
+                  double x = (-b + Math.sqrt(dis (a, b, c))) / (2 * a);
                   System.out.println("У выражения один корен " + x);
               }
               else {
-                  if (res<0) {
+                  if (dis (a, b, c)<0) {
                       System.out.println("У выражения нет корней");
                   }
               }
         }
     }
 
-    private static double dis (double a, double b, double c) {
+    private static double dis (int a, int b, int c) {
         double D = b * b - 4 * a * c;
         return D;}
     }
