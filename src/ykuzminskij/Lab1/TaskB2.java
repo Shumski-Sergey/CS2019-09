0abd07e166c2b3b96703d73bfebf224a34cf5a2d
Yury6720 <kuzminskij5683415@gmail.com>
2019-09-10 21:14:03 +0300

    Converter Celsium to Fahrenheit

--- src/ykuzminskij/Lab1/TaskB2.java
+++ src/ykuzminskij/Lab1/TaskB2.java
@@ -6,7 +6,7 @@ public class TaskB2 {
     private static int c=9;
     private static int d=27;
 
-    public static void main (String[]args) {
+    public static void main (String[] args) {
         int result = - a + b - c + d;
         System.out.println(result);
     }
---
+++ src/ykuzminskij/Lab3/TaskA1.java
@@ -0,0 +1,12 @@
+package ykuzminskij.Lab3;
+
+public class TaskA1 {
+    public static void main (String [] args) {
+        int i=0;
+        if (i<10){
+            System.out.println("Я изучаю програмировние");
+            i++;
+        }
+    }
+
+}
