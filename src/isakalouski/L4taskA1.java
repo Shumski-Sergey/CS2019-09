dc421bc541686935b3f168b17e16959eaa3d2831
ilya-sakalouski <ilya6697089@mail.ru>
2019-09-11 19:45:17 +0300

    LESSON 4 (А1 & А2)

---
+++ src/isakalouski/L4taskA1.java
@@ -0,0 +1,14 @@
+package isakalouski;
+
+public class L4taskA1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10; i++) {
+            if (i==3 || i==9) {
+                System.out.println("Я хочу получать высокую зарплату!");
+            }
+            else {
+                System.out.println("Я начинаю изучать Java");
+            }
+        }
+    }
+}
---
+++ src/isakalouski/L4taskA2.java
@@ -0,0 +1,35 @@
+package isakalouski;
+
+import java.util.Scanner;
+
+public class L4taskA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        if (number < 1 || number > 7) {
+            System.out.println("Такого дня недели не существует!");
+        }
+        if (number == 1) {
+            System.out.println("Понедельник");
+        }
+        if (number == 2) {
+            System.out.println("Вторник");
+        }
+        if (number == 3) {
+            System.out.println("Среда");
+        }
+        if (number == 4){
+            System.out.println("Четверг");
+        }
+        if (number == 5) {
+            System.out.println("Пятница");
+        }
+        if (number == 6) {
+            System.out.println("Суббота");
+        }
+        if ( number == 7) {
+            System.out.println("Воскресенье");
+        }
+    }
+}
