2046fe8188b7d7130dc5140bec689adbf80b9fb4
Yury6720 <kuzminskij5683415@gmail.com>
2019-09-11 23:34:35 +0300

    Program output mantra

--- src/ykuzminskij/Lab3/TaskA1.java
+++ src/ykuzminskij/Lab3/TaskA1.java
@@ -1,13 +1,10 @@
-//package ykuzminskij.Lab3;
+package ykuzminskij.Lab3;
 
-//public class TaskA1 {
-  //  public static void main (String [] args) {
-    //    for (int i = 0; i < 10; i++) {
-      //      System.out.println("Я изучаю програмировние");
-        //    if (i=3; i=9) {
-          //      System.out.println("я хочу получать большую зарплату");
-            //}
-        //}
-    //}
-
-//}
+public class TaskA1 {
+    public static void main (String [] args) {
+        for (int i = 0; i < 10; i++) {
+            if (!(i==2) && !(i == 8)) {System.out.println("Я изучаю програмировние");}
+            else System.out.println("я хочу получать большую зарплату");
+            }
+    }
+}
