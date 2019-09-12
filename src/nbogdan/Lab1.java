0d5cd75f7ece24eb917e0ee9ce4485c392903654
Woof <7883494@gmail.com>
2019-09-12 03:10:06 +0300

    1

--- src/nbogdan/Lab1.java
+++
@@ -1,8 +0,0 @@
-package nbogdan;
-
-public class Lab1 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-    }
-}
---
+++ src/nbogdan/Lesson3/TaskB/TaskB1.java
@@ -0,0 +1,27 @@
+package nbogdan.Lesson3.TaskB;
+import java.util.Scanner;
+
+public class TaskB1 {
+    private static int metodyear(int year) {
+        if (!(year % 400 == 0) && (year % 4 == 0)) {
+            System.out.println("Год невисокосный.");
+            year = 365;
+            return year;
+        } else if (year % 4 == 0) {
+            System.out.println("Год високосный.");
+            year = 366;
+            return year;
+        } else {
+            System.out.println("Год невисокосный.");
+            year = 365;
+            return year;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите любой год нашей эры, чтобы узнать, сколько в нем дней ");
+        int year = sc.nextInt();
+        System.out.println(metodyear(year));
+    }
+}
